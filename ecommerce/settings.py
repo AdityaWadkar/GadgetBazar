14bb8d7fb0a4a2706ef3d85c32d9e5f6449e48b1
Aditya Wadkar <wadkaraditya923@gmail.com>
2023-09-04 18:51:11 +0530

    adding new collect static

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -139,9 +139,9 @@ EMAIL_HOST_PASSWORD = "kcanlwngsmjjlfbt"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 import os
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
 
 
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -23,6 +23,10 @@ urlpatterns = [
     path('', include("ecommerceapp.urls")),
     path('auth/', include("authcart.urls")),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+ urlpatterns += path('admin/', admin.site.urls)
 
 
