8f464a287faf9e99f0becaa8f982fda50da13f1b
Aditya Wadkar <wadkaraditya923@gmail.com>
2023-09-04 19:01:33 +0530

    adding new collect static

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -19,14 +19,14 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include("ecommerceapp.urls")),
     path('auth/', include("authcart.urls")),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
- urlpatterns += path('admin/', admin.site.urls)
+# if settings.DEBUG:
+#  urlpatterns += path('admin/', admin.site.urls)
 
 
